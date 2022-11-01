afa668677f9e037831eb9c2ca708251b6fc6f956
unknown <Srikaran9876@gmail.com>
2022-11-01 16:58:23 +0530

    Linked list

---
+++ DAKR_RND/src/CollectionsAndBasics/Linked_list.java
@@ -0,0 +1,30 @@
+package CollectionsAndBasics;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Linked_list {
+		public static void main(String[] args) {
+			LinkedList<String>al=new LinkedList<String>();
+			al.add("Ravi");
+			al.add("Teja");
+			al.add("abhi");
+			al.addFirst("akhil");
+			al.addLast("Pokuri");
+			al.add(2,"raju");
+			LinkedList<String>al1=new LinkedList<String>();
+			al1.add("Gaurav");
+			al1.add("srinu");
+			al.addAll(al1);
+			al.remove("Ravi");
+			al.remove(3);
+			al.removeFirst();
+			al.removeLast();
+			al.removeLastOccurrence("Gaurav");
+			Iterator<String>itr=al.iterator();
+			while(itr.hasNext()) {
+				System.out.println(itr.next());
+			}
+
+		}
+
+	}
