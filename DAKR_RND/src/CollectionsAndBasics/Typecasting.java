d30d2b82fe4aa625c51e262d737b920239458418
sai sreeja <Rajesh7730000373.com>
2022-11-02 15:06:27 +0530

    Type casting

---
+++ DAKR_RND/src/CollectionsAndBasics/Typecasting.java
@@ -0,0 +1,17 @@
+package CollectionsAndBasics;
+
+public class Typecasting {
+
+	
+		public static void main(String[] args) {  //Widening casting
+		    int myInt = 9;
+		    double myDouble = myInt; // Automatic casting: int to double
+
+		    System.out.println(myInt);      // Outputs 9
+		    System.out.println(myDouble);   // Outputs 9.0
+		}
+  
+            
+	
+	}
+  
---
+++ DAKR_RND/src/CollectionsAndBasics/Typecastingnarow.java
@@ -0,0 +1,17 @@
+package CollectionsAndBasics;
+
+public class Typecastingnarow {
+
+	public static void main(String[] args) {
+	
+		
+			    double myDouble = 9.78d;
+			    int myInt = (int) myDouble; // Manual casting: double to int
+
+			    System.out.println(myDouble);   // Outputs 9.78
+			    System.out.println(myInt);      // Outputs 9
+			  }
+
+	}
+
+
