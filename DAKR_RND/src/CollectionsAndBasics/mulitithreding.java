3f2a1bb6c96e5a0a8ed52035ee9cafef55a16552
sai sreeja <Rajesh7730000373.com>
2022-11-02 16:09:52 +0530

    mtr

---
+++ DAKR_RND/src/CollectionsAndBasics/mulitithreding.java
@@ -0,0 +1,32 @@
+package CollectionsAndBasics;
+
+	 class dummy extends Thread {
+	    public void run()
+	    {
+	        try {
+
+	            System.out.println(
+	                "Thread " + Thread.currentThread().getId()
+	                + " is running");
+	        }
+	        catch (Exception e) {
+	          
+	            System.out.println("Exception is caught");
+	        }
+	    }
+	}
+
+	public class mulitithreding {
+	    public static void main(String[] args)
+	    {
+	        int n = 8;
+	        for (int i = 0; i < n; i++) {
+	        	dummy  object = new dummy ();
+	            object.start();
+
+	        }
+	    }
+	}
+	
+
+
