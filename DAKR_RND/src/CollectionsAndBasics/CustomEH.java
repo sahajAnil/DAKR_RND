1ab2e774b3e385579f76c648ee7b5d356497a6c3
naveen5500 <chattanaveen8@gmail.com>
2022-11-03 15:06:28 +0530

    Try $ Catch

--- DAKR_RND/src/CollectionsAndBasics/CustomEH.java
+++ DAKR_RND/src/CollectionsAndBasics/CustomEH.java
@@ -1,17 +1,59 @@
 package CollectionsAndBasics;
 
-public class CustomEH {
+
+	
+	class InvalidAgeException  extends Exception  
+	{  
+	    public InvalidAgeException (String str)  
+	    {  
+	        // calling the constructor of parent Exception  
+	        super(str);  
+	    }  
+	} 
+         
+	public class CustomEH {
+		 // method to check the age  
+		 static void validate (int age) throws InvalidAgeException{    
+		    if(age < 18){  
+
+		     // throw an object of user defined exception  
+		     throw new InvalidAgeException("age is not valid to vote");    
+		     }  
+		    else {   
+		     System.out.println("welcome to vote");   
+		     }
+		 }
+
 
 	public static void main(String[] args) {
 		
-      int age=19;
+     /* int age=19;
       if(age>18) {
     	 System.out.println("vote is Elegible");
       }
       else
       {
     	  System.out.println("vote is not Elegible");
-      }
+      }*/
+	
+		
+		try  
+        {  
+            // calling the method   
+            validate(19);  
+        }  
+        catch (InvalidAgeException ex)  
+        {  
+            System.out.println("Caught the exception");  
+    
+            // printing the message from InvalidAgeException object  
+            System.out.println("Exception occured: " + ex);  
+        }  
+  
+        System.out.println();
+
+		
 	}
+	
 
 }
