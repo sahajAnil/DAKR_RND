5646a5ceadb86bb539d134c1e8934195b394a7c5
sai sreeja <Rajesh7730000373.com>
2022-11-07 14:43:15 +0530

    cach

---
+++ DAKR_RND/src/CollectionsAndBasics/cachprogram.java
@@ -0,0 +1,22 @@
+package CollectionsAndBasics;
+
+public class cachprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10 ;
+        int b = 0;
+        int c = 0 ;
+        try {
+        	c = a/b;
+        	
+        }catch(Exception e ) {
+        
+        
+        System.out.println(c);
+        int d = 10 + c;
+        System.out.println(d);
+	}
+
+}
+}
---
+++ DAKR_RND/src/CollectionsAndBasics/queueprogram.java
@@ -0,0 +1,10 @@
+package CollectionsAndBasics;
+
+public class queueprogram {
+
+	public static void main(String[] args) {
+		System.out.println);
+
+	}
+
+}
