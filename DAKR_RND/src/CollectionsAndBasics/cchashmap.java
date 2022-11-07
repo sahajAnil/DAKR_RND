b05af9169f3de7df4305ddd8dbc31a568757afe6
sai sreeja <Rajesh7730000373.com>
2022-11-07 14:50:25 +0530

    cchashmap

--- DAKR_RND/src/CollectionsAndBasics/cchashmap.java
+++ DAKR_RND/src/CollectionsAndBasics/cchashmap.java
@@ -18,18 +18,18 @@ public class cchashmap {
 		        my_cmmap.put("5", "1");
 		        my_cmmap.put("6", "1");
 		 
-		        // Printing the map
+		       
 		        System.out.println("Mappings of my_cmmap : "
 		                           + my_cmmap);
 		 
-		        // create another concurrentHashMap
+		        
 		        ConcurrentHashMap<String, String> new_chm
 		            = new ConcurrentHashMap<>();
 		 
-		        // copy mappings from my_cmmap to new_chm
+		        
 		        new_chm.putAll(my_cmmap);
 		 
-		        // Displaying the new map
+		        
 		        System.out.println("New mappings are: " + new_chm);
 		    }
 		}
