9b6854c0b5a6ab4d516a17fe8618d7d6c1ad6060
naveen5500 <chattanaveen8@gmail.com>
2022-11-09 21:17:09 +0530

    Palindrome String

--- DAKR_RND/src/BasicsCode/PalindromeString.java
+++ DAKR_RND/src/BasicsCode/PalindromeString.java
@@ -3,7 +3,22 @@ package BasicsCode;
 public class PalindromeString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String s= " Naveen Navi 5500 ",reverses=" ";
+		int sLength =s.length();
+		
+		for(int i=(sLength=1);i>0;--i) {
+			
+			reverses=reverses +s.charAt(i);
+			
+		}
+		if(s.toLowerCase().equals(reverses.toLowerCase())) {
+			System.out.println(s+" This is Palindrime String");
+			
+		}
+		else {
+			System.out.println(s+"This is not a Palindrime String");
+		}
 
 	}
 
