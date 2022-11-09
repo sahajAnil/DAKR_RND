c96c2f04cdc0d32cd1102047332901847e7f160f
unknown <prajeethkumardharapogu@gmail.com>
2022-11-09 17:50:28 +0530

    Collections

---
+++ DAKR_RND/src/CollectionsAndBasics/Collections.java
@@ -0,0 +1,11 @@
+package CollectionsAndBasics;
+import java.util.LinkedList;
+public class Collections {
+           public static void main(String[] args){
+            LinkedList<String> animals = new LinkedList<>();
+		    animals.add("Dog");
+		    animals.add("Cat");
+		    animals.add("Cow");
+		    System.out.println("LinkedList: " + animals);
+		  }
+		}
