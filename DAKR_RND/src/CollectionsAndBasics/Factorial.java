00c586dcc9769a982ef1e6a24ca450a6cb2fd043
unknown <prajeethkumardharapogu@gmail.com>
2022-11-09 17:54:27 +0530

    Factorial

---
+++ DAKR_RND/src/CollectionsAndBasics/Factorial.java
@@ -0,0 +1,21 @@
+package CollectionsAndBasics;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+			        int num = 10;
+			        long factorial = 1;
+			        for(int i = 1; i <= num; ++i)
+			        {
+			            // factorial = factorial * i;
+			            factorial *= i;
+			        }
+			        System.out.println("Factorial of "+ num+"="+ factorial);
+			    }
+			}
+
+
+	
+
+
