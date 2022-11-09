530a9c1ce8aa03153b479627aa5b3867a167d555
unknown <prajeethkumardharapogu@gmail.com>
2022-11-09 17:31:40 +0530

    main

---
+++ DAKR_RND/src/CollectionsAndBasics/wrapper.java
@@ -0,0 +1,18 @@
+package CollectionsAndBasics;
+
+import java.util.Scanner;
+
+public class wrapper {
+
+	public static void main(String[] args) {
+		 Scanner in= new Scanner (System.in);
+		 System.out.println("enter first number");
+		 Integer s = in.nextInt();
+		 System.out.println("enter second number");
+		 Integer p = in.nextInt();
+		 Integer j =s+p;
+		 System.out.println("add="+j);
+
+	}
+
+}
