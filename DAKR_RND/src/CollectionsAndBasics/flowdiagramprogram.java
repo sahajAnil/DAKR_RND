f410b875e1def75e5414b65c4976e4848e3f0810
sai sreeja <Rajesh7730000373.com>
2022-11-11 16:00:24 +0530

    queue

---
+++ DAKR_RND/src/CollectionsAndBasics/flowdiagramprogram.java
@@ -0,0 +1,16 @@
+package CollectionsAndBasics;
+
+public class flowdiagramprogram {
+
+	public static void main(String[] args) {
+		 String abc = "Sat";
+		 if (abc == "Mon");{
+			 System.out.println("monday");
+		 }
+		else if (abc == "Tue"); 
+			 
+		 
+	
+}
+
+}
