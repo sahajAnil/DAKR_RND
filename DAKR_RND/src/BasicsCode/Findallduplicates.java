9f2fee70c869cca38f47656d32ca8b0dbb600962
naveen5500 <chattanaveen8@gmail.com>
2022-11-12 16:49:14 +0530

    GFG

---
+++ DAKR_RND/src/BasicsCode/Findallduplicates.java
@@ -0,0 +1,23 @@
+package BasicsCode;
+
+public class Findallduplicates {
+
+	public static void main(String[] args) {
+		 
+		   String str = "Navi Mahi NagaLakshmi";
+		      char[] carray = str.toCharArray();
+		    System.out.println("The string is:" + str);
+	  System.out.print("Duplicate Characters in above string are: ");
+	    for (int i = 0; i < str.length(); i++) {
+	         for (int j = i + 1; j < str.length(); j++) {
+	            if (carray[i] == carray[j]) {
+	               System.out.print(carray[j] + " ");
+	               break;
+		  
+		            }
+		         }
+	    }
+
+	}
+
+}
---
+++ DAKR_RND/src/BasicsCode/lf.java
@@ -0,0 +1,16 @@
+package BasicsCode;
+
+public class lf {
+
+	public static void main(String[] args) {
+		
+		int a=40;
+		int b=30;
+		if (a<b)
+		System.out.println("dakr solutions");
+		else 
+		System.out.println(" naga lakshmi");
+
+	}
+
+}
